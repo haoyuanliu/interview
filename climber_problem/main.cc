84385e943f604ad6e0a6c04241c3f8a4ff317f46
haoyuanliu <15120101@bjtu.edu.cn>
2017-03-08 20:11:31 +0800

    update

--- climber_problem/main.cc
+++ climber_problem/main.cc
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <iostream>
 #include <sstream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <queue>
 #include <assert.h>
 
 #define H_ARRAYSIZE(a) \
@@ -12,37 +16,44 @@ int resolve(const char* input)
     int res = 0;
     std::istringstream strs(input);
     std::string str;
-    int len;
+    std::vector<std::pair<int, int> > heights;
+    int len, left, right, height;
     strs >> len;
-    int left = 0;
-    int right = 0;
-    int start = 0;
-    int end = 0;
-    int height = 0;
-    int maxHeight = 0;
     while(strs >> str)
     {
         left = atoi(str.substr(0, str.find(',')).data());
         right = atoi(str.substr(str.find(',')+1, str.rfind(',')).data());
         height = atoi(str.substr(str.rfind(',')+1).data());
-        
-        if(left > end)
-        {
-            res += maxHeight * 2;
-            start = left;
-            end = right;
-            maxHeight = height;
-        }
+        heights.push_back(std::make_pair(left, -height));
+        heights.push_back(std::make_pair(right, height));
+    }
+    std::sort(heights.begin(), heights.end());
+    std::map<int, int> m;
+    std::priority_queue<int> q;
+    q.push(0);
+    int pre = 0;
+    res += heights[len * 2 - 1].first;
+
+    for(int i = 0; i < len * 2; ++i)
+    {
+        if(heights[i].second < 0)
+            q.push(-heights[i].second);
         else
         {
-            start = std::min(start, left);
-            end = std::max(end, right);
-            maxHeight = std::max(maxHeight, height);
+            ++m[heights[i].second];
+            while(!q.empty() && m[q.top()] > 0)
+            {
+                --m[q.top()];
+                q.pop();
+            }
+        }
+        int cur = q.top();
+        if(cur != pre)
+        {
+            res += std::abs(pre - cur);
+            pre = cur;
         }
     }
-    res += maxHeight * 2;
-    res += end;
-    std::cout << res << std::endl;
     return res;
 }
 
@@ -65,8 +76,8 @@ int main(int argc, char* argv[])
     int expectedSteps[] = {25, 4, 7, 10, 14, 15, 3, 12, 13, 14, 20};
     for (size_t i = 0; i < H_ARRAYSIZE(input); ++i)
     {
-        resolve(input[i]);
-        //assert(resolve(input[i]) == expectedSteps[i]);
+        assert(resolve(input[i]) == expectedSteps[i]);
     }
+    std::cout << "Finished!" << std::endl;
     return 0;
 }
