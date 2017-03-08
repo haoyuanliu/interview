8a2e1341c07c5e260fc55a20014a142e0d86233e
haoyuanliu <15120101@bjtu.edu.cn>
2017-03-08 20:15:44 +0800

    update

--- climber_problem/main.cc
+++ climber_problem/main.cc
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <assert.h>
+
 #include <iostream>
 #include <sstream>
 #include <vector>
 #include <map>
 #include <algorithm>
 #include <queue>
-#include <assert.h>
+
 
 #define H_ARRAYSIZE(a) \
     ((sizeof(a) / sizeof(*(a))) / \
@@ -24,15 +26,16 @@ int resolve(const char* input)
         left = atoi(str.substr(0, str.find(',')).data());
         right = atoi(str.substr(str.find(',')+1, str.rfind(',')).data());
         height = atoi(str.substr(str.rfind(',')+1).data());
+        
         heights.push_back(std::make_pair(left, -height));
         heights.push_back(std::make_pair(right, height));
     }
     std::sort(heights.begin(), heights.end());
+    res += heights[len * 2 - 1].first;
     std::map<int, int> m;
     std::priority_queue<int> q;
     q.push(0);
     int pre = 0;
-    res += heights[len * 2 - 1].first;
 
     for(int i = 0; i < len * 2; ++i)
     {
@@ -59,7 +62,8 @@ int resolve(const char* input)
 
 int main(int argc, char* argv[])
 {
-    const char* input[] = {
+    const char* input[] =
+    {
         "3\n1,3,2\n2,4,4\n6,7,5\n", //The giving example
         "1\n1,2,1\n",
         "2\n1,2,1\n2,3,2",
@@ -72,7 +76,8 @@ int main(int argc, char* argv[])
         "4\n0,1,1\n2,4,3\n3,5,1\n5,6,1",
         "5\n0,1,1\n2,4,3\n3,5,1\n5,6,1\n6,8,3",
         //TODO please add more test case here
-        };
+    };
+
     int expectedSteps[] = {25, 4, 7, 10, 14, 15, 3, 12, 13, 14, 20};
     for (size_t i = 0; i < H_ARRAYSIZE(input); ++i)
     {
